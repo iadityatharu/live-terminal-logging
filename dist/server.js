04d3acee348df4d91038c6965d1fd16ec15b7fa7
iadityatharu <adityatharudev@gmail.com>
2025-04-16 23:17:26 +0545

    feat:tested a package

--- dist/server.js
+++ dist/server.js
@@ -22,6 +22,11 @@ app.get("/", (_req, res) => {
 app.post("/error", (_req, res) => {
   res.status(500).json({ error: "Oops! Something went wrong." });
 });
+app.patch("/patch", (_req, res) => {
+  setTimeout(() => {
+    res.status(200).json({ message: "Patch request successful." });
+  }, 1000);
+});
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
 });
